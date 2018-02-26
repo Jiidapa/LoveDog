26ebe92ff5fec8c0944068bdb0f40013cdbe159d
Jidapa <jidapa.p@kkumail.com>
2018-02-26 21:55:23 +0700

    eiei

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-$pdo = new PDO("mysql:host=localhost;dbname=sec01_bs;charset=utf8", "Sec01_BS", "S4b31St6");
+$pdo = new PDO("mysql:host=10.199.66.27;dbname=sec01_bs;charset=utf8", "Sec01_BS", "S4b31St6");
 } catch (PDOException $e) {
 echo "เกิดข้อผิดพลาด : " . $e->getMessage();
 }
