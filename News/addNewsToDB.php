398a5c41792cc400b0391387ae1862d9cf231a37
Jidapa <jidapa.p@kkumail.com>
2018-02-27 14:46:37 +0700

    new commit

--- News/addNewsToDB.php
+++ News/addNewsToDB.php
@@ -3,10 +3,15 @@ include "../connect.php"
 ?>
 
 <?php 
-$stmt = $pdo->prepare("INSERT INTO news VALUES ('', ?, ?, ?)");
-$stmt->bindParam(1, $_POST["Topic"]);
-$stmt->bindParam(2, $_POST["Content"]);
-$stmt->bindParam(3, $_POST["Date"]);
+chmod("../img/imgAnno", 777);
+$topic = $_POST['Topic'];
+$content = $_POST['Content'];
+$date = $_POST['Date'];
+
+$stmt = $pdo->prepare("INSERT INTO news  (Topic, Content, Date) VALUES (?, ?, ?)");
+$stmt->bindParam(1, $topic);
+$stmt->bindParam(2, $content);
+$stmt->bindParam(3, $date);
 $stmt->execute(); 
 ?>
 
@@ -18,20 +23,60 @@ $stmt->execute();
 $row= $stmt->fetch();
 $newsid= $row["NewsId"];
 
+// $uploads_dir = "../img/imgAnno";
+// foreach ($_FILES["ImageName"]["error"] as $key => $error) {
+//     if ($error == UPLOAD_ERR_OK) {
+//         $tmp_name = $_FILES["ImageName"]["tmp_name"][$key];
+//         $name = basename($_FILES["ImageName"]["name"][$key]);
+//         $uploadfile = $uploads_dir .basename($_FILES["ImageName"]["name"][$key]);
+//         $stmt = $pdo->prepare("INSERT INTO images (ImgName,Date,NewsId) VALUES (?, ?, ?)");
+//         $stmt->bindParam(1, $name);
+//         $stmt->bindParam(2, $date);
+//         $stmt->bindParam(3, $newsid);
+//         try {
+//             $stmt->execute();
+//             print_r($tmp_name);
+//             echo "Dir: " . $uploads_dir;
+//             if(move_uploaded_file($tmp_name, $uploadfile)) {
+//                 echo "Upload " .basename($_FILES["ImageName"]["name"][$key]) ." complete.<br>";
+//             } else {
+//                 echo "Upload failed.<br>";
+//             }
+//             echo "ชื่อรูป :".$name;
+//             echo "ไอดีข่าว :".$newsid;
+//             echo "วันที่ :".$date;
+            
+//         } catch (PDOException $e) {
+//             echo "Error " .$e->getMessage();
+//         }
+//     }
+    
+//    // header('Location: http://10.199.66.227/SoftEn2018/Sec01_BS/News/FormNews.php');
+// }
+
 $uploads_dir = '../img/imgAnno';
 foreach ($_FILES["ImageName"]["error"] as $key => $error) {
     if ($error == UPLOAD_ERR_OK) {
         $tmp_name = $_FILES["ImageName"]["tmp_name"][$key];
         $name = basename($_FILES["ImageName"]["name"][$key]);
         echo $name;
-        $stmt = $pdo->prepare("INSERT INTO images VALUES ('', ?, ?, ?) ");
+        $stmt = $pdo->prepare("INSERT INTO images (ImgName,Date,NewsId) VALUES (?, ?, ?)");
         $stmt->bindParam(1, $name);
-        $stmt->bindParam(2, $_POST["Date"]);
+        $stmt->bindParam(2, $date);
         $stmt->bindParam(3, $newsid);
         $stmt->execute();
-        move_uploaded_file($tmp_name, "$uploads_dir/$name");
+        if(move_uploaded_file($tmp_name, "$uploads_dir/$name")){
+            echo "up com";
+        }else{
+            echo "fail";
+        }
+        
     }
 }
+
+
+
+
 /*
 foreach ($_FILES['img']['name'] as $file)
     {
